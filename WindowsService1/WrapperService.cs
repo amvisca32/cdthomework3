1802666943690ef42a64e8a445838e82cc95fc2b
amvisca32 <amvisca32@gmail.com>
2019-02-17 22:05:03 -0500

    Update WrapperService.cs

--- WindowsService1/WrapperService.cs
+++ WindowsService1/WrapperService.cs
@@ -1,48 +1,70 @@
-﻿using System;
+///
+/// WrapperService.cs
+/// CDT Homework #3
+/// Author: Adrianna Visca
+/// 
+
+using System;
 using System.IO;
 using System.ServiceProcess;
 using System.Timers;
-using System.Windows.Forms;
 
 namespace WindowsService1
 {
+    /// <summary>
+    /// Contains methods for starting, stopping, and recalling
+    /// a windows service. Uses the base class ServiceBase.
+    /// </summary>
     public partial class WrapperService : ServiceBase
     {
-            System.Timers.Timer timer = new System.Timers.Timer(); // name space(using System.Timers;) 
-            //KeyboardActions key = new KeyboardActions();
+            Timer timer = new Timer(); // name space(using System.Timers;) 
+            
+            /// <summary>
+            /// Constructor for the WrapperService that calls InializeCompnent method
+            /// which is the required method for designer support.
+            /// </summary>
             public WrapperService()
             { 
                 InitializeComponent();
             }
 
+            /// <summary>
+            /// Called when the service is started either from the command line or
+            /// from the Services GUI. Writes what time the service was started in the log
+            /// file and sets the recall time to 5 seconds.
+            /// </summary>
             protected override void OnStart(string[] args)
             {
                 WriteToFile("Service is started at " + DateTime.Now);
                 timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-                timer.Interval = 20000; //5000; number in milisecinds  
+                timer.Interval = 5000; //number in milisecinds  
                 timer.Enabled = true;
             }
 
+            /// <summary>
+            /// Called when the service is stopped either from the command line or
+            /// from the Services GUI. Writes what time the service was stopped in the log
+            /// file.
+            /// </summary>
             protected override void OnStop()
             {
                 WriteToFile("Service is stopped at " + DateTime.Now);
             }
 
+            /// <summary>
+            /// Called every 5 seconds when the service is recalled. Writes what time the 
+            /// service was recalled in the log file.
+            /// </summary>
             private void OnElapsedTime(object source, ElapsedEventArgs e)
             {
-                WriteToFile("Service is recall at " + DateTime.Now);
-                //string text = "Shark";
-                //MessageBox.Show(text);
-                DisplayMessageBoxText();
+            WriteToFile("Service is recall at " + DateTime.Now);
             }
 
-            private void DisplayMessageBoxText()
-            {
-                MessageBox.Show("Hello, world.");
-            }
-
-
-        public void WriteToFile(string Message)
+            /// <summary>
+            /// Function to write to the log file within the debug file using the message
+            /// provided as a parameter.
+            /// </summary>
+            public void WriteToFile(string Message)
             {
                 string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
                 if (!Directory.Exists(path))
@@ -68,3 +90,4 @@ namespace WindowsService1
             }
     }
 }
+
